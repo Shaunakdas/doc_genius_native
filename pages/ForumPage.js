0f6ecb3047f8aa284cc897d1b6264150ae728686
Subramanyam S <shastri9999@gmail.com>
2017-09-14 09:47:49 +0530

    Making forum change

--- pages/ForumPage.js
+++ pages/ForumPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Image, TextInput } from 'react-native';
+import { Text, View, Image, TextInput, ScrollView } from 'react-native';
 import { PropTypes } from 'prop-types';
 
 import IMAGES from '../common/images';
@@ -29,6 +29,305 @@ export default class ForumPage extends React.Component {
     navigation.navigate('CategorySelectionPage', { fromForum: true });
   }
 
+  renderCategoryLabel(category) {
+    return (<View style={{
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+    }}
+    >
+      <View style={{
+        backgroundColor: COLORS.PRIMARY,
+        borderRadius: 5,
+        paddingHorizontal: 8,
+        paddingVertical: 3,
+        shadowColor: alpha(COLORS.BLACK, 0.4),
+        shadowOffset: { height: 3 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3,
+        elevation: 2,
+      }}
+      >
+        <Text style={{
+          ...font(12),
+          color: COLORS.WHITE,
+          backgroundColor: COLORS.TRANSPARENT,
+        }}
+        >
+          {category}
+        </Text>
+      </View>
+    </View>);
+  }
+
+  renderQ(question) {
+    return (
+      <View style={{
+        padding: 5,
+        paddingRight: 8,
+      }}
+      >
+        <Text
+          style={{
+            position: 'absolute',
+            top: -8,
+            left: -15,
+            ...font(40),
+            color: alpha(COLORS.SECONDARY, 0.3),
+          }}
+        >
+        Q
+        </Text>
+        <View style={{
+          borderBottomWidth: 1,
+          borderColor: COLORS.SECONDARY,
+          paddingLeft: 15,
+          paddingBottom: 4,
+        }}
+        >
+          <Text
+            numberOfLines={2}
+            style={{
+              ...font(13),
+              marginBottom: 8,
+            }}
+          >
+            {question.text}
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }} >
+              <Image
+                source={IMAGES.POST_BY}
+                style={{
+                  height: 16,
+                  width: 16,
+                  resizeMode: 'contain',
+                }}
+              />
+              <Text
+                style={{
+                  ...font(13),
+                  marginLeft: 6,
+                }}
+              >
+                {question.user}
+              </Text>
+            </View>
+            <Text style={{
+              flex: 1,
+              marginHorizontal: 8,
+              ...font(8),
+              color: COLORS.SECONDARY,
+            }}
+            >
+              {question.ago}
+            </Text>
+            <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center' }}>
+              <Text
+                style={{
+                  ...font(9),
+                  marginRight: 3,
+                  marginLeft: 10,
+                  color: COLORS.SECONDARY,
+                }}
+              >
+                {question.likes}
+              </Text>
+              <Image
+                source={IMAGES.HEART}
+                style={{
+                  height: 16,
+                  width: 16,
+                  resizeMode: 'contain',
+                  marginRight: 5,
+                }}
+              />
+              <Text
+                style={{
+                  ...font(9),
+                  marginRight: 3,
+                  marginLeft: 10,
+                  color: COLORS.SECONDARY,
+                }}
+              >
+                {question.replies}
+              </Text>
+              <Image
+                source={IMAGES.ANSWERS}
+                style={{
+                  height: 16,
+                  width: 16,
+                  resizeMode: 'contain',
+                  marginRight: 5,
+                }}
+              />
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  }
+
+  renderA(answer, key, hideBorder = false) {
+    return (
+      <View
+        style={{
+          padding: 5,
+          paddingRight: 8,
+        }}
+        key={key}
+      >
+        <Text
+          style={{
+            position: 'absolute',
+            top: -8,
+            left: -15,
+            ...font(40),
+            color: alpha(COLORS.SECONDARY, 0.3),
+          }}
+        >
+        A
+        </Text>
+        <View style={{
+          borderBottomWidth: hideBorder ? 0 : 1,
+          borderColor: COLORS.SECONDARY,
+          paddingLeft: 15,
+          paddingBottom: 4,
+        }}
+        >
+          <Text
+            numberOfLines={2}
+            style={{
+              ...font(13),
+              marginBottom: 8,
+            }}
+          >
+            {answer.text}
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }} >
+              <Image
+                source={IMAGES.ANSWER_BY}
+                style={{
+                  height: 16,
+                  width: 16,
+                  resizeMode: 'contain',
+                }}
+              />
+              <Text
+                style={{
+                  ...font(13),
+                  marginLeft: 6,
+                }}
+              >
+                {answer.user}
+              </Text>
+            </View>
+            <Text style={{
+              flex: 1,
+              marginHorizontal: 8,
+              ...font(8),
+              color: COLORS.SECONDARY,
+            }}
+            >
+              {answer.ago}
+            </Text>
+            <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center' }}>
+              <Text
+                style={{
+                  ...font(9),
+                  marginRight: 3,
+                  marginLeft: 10,
+                  color: COLORS.SECONDARY,
+                }}
+              >
+                {answer.likes}
+              </Text>
+              <Image
+                source={IMAGES.HEART}
+                style={{
+                  height: 16,
+                  width: 16,
+                  resizeMode: 'contain',
+                  marginRight: 5,
+                }}
+              />
+              <Text
+                style={{
+                  ...font(9),
+                  marginRight: 3,
+                  marginLeft: 10,
+                  color: COLORS.SECONDARY,
+                }}
+              >
+                {answer.replies}
+              </Text>
+              <Image
+                source={IMAGES.ANSWERS}
+                style={{
+                  height: 16,
+                  width: 16,
+                  resizeMode: 'contain',
+                  marginRight: 5,
+                }}
+              />
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  }
+  renderQA() {
+    const question = {
+      id: 1,
+      text: 'What is UCLA\'s average ACT score?',
+      role: 'student',
+      user: 'Aaron',
+      ago: '5 hours ago',
+      category: 'SAT/ACT/AP',
+      liked: false,
+      likes: 3,
+      replies: 10,
+      answers: [{
+        text: 'Some logical answer, can be spanned into multiple lines',
+        user: 'Greg',
+        grade: '11th',
+        likes: 3,
+        replies: 3,
+        role: 'student',
+        ago: '3 hours ago',
+      }],
+    };
+    const lastAnswerIndex = question.answers.length - 1;
+    return (
+      <View
+        style={{
+          margin: 8,
+          backgroundColor: COLORS.WHITE,
+          borderRadius: 10,
+          padding: 6,
+          overflow: 'hidden',
+        }}
+      >
+        {this.renderCategoryLabel(question.category)}
+        {this.renderQ(question)}
+        {question.answers.map(
+          (answer, index) => this.renderA(answer, index, index === lastAnswerIndex),
+        )}
+      </View>
+    );
+  }
   render() {
     return (
       <View
@@ -106,6 +405,11 @@ export default class ForumPage extends React.Component {
             />
           </View>
         </View>
+        <ScrollView
+          style={{ flex: 1 }}
+        >
+          {this.renderQA()}
+        </ScrollView>
       </View>
     );
   }
