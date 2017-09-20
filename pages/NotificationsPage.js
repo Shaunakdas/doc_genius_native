1e922751e706c665680df463719e0e65df4bf929
Subramanyam S <shastri9999@gmail.com>
2017-09-21 02:09:08 +0530

    Adding notifications scroll

--- pages/NotificationsPage.js
+++ pages/NotificationsPage.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
 import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
+import moment from 'moment';
 
 import IMAGES from '../common/images';
 import COLORS from '../common/colors';
 import { IconButton } from '../components';
 import { commonStyle as cs, font } from '../common/styles';
 
-export default class NotificationsPage extends React.Component {
+class NotificationsPage extends React.Component {
   static propTypes = {
     navigation: PropTypes.object.isRequired,
+    notifications: PropTypes.array.isRequired,
   }
 
   constructor(props) {
     super(props);
     this.state = {
-      profile: null,
+      loadingNotifications: true,
     };
   }
 
@@ -24,7 +27,61 @@ export default class NotificationsPage extends React.Component {
     navigation.goBack();
   }
 
+  gotoQuestionPage = (notification) => {
+    const { navigation } = this.props;
+    if (notification.topic_id) {
+      navigation.navigate('NotificationQuestionPage', { id: notification.topic_id });
+    }
+  }
+
+  goToNotification = notification => () => {
+    this.gotoQuestionPage(notification);
+  }
+
+  renderNotification = (notification) => {
+    const actions = {
+      5: 'liked your post',
+      9: 'replied to your post',
+      13: 'created a new question',
+    };
+    return (
+      <TouchableOpacity
+        onPress={this.goToNotification(notification)}
+        key={notification.id}
+      >
+        <View
+          style={{
+            marginVertical: 8,
+            borderBottomWidth: 1,
+            borderColor: COLORS.SECONDARY,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <View>
+            <Text style={{ marginLeft: 15, ...font(13) }}>
+              {`${notification.userFullName} ${actions[notification.type]}`}
+            </Text>
+            <Text style={{ marginLeft: 15, ...font(10), marginVertical: 3 }}>
+              {moment(notification.created_at).fromNow()}
+            </Text>
+          </View>
+          <Image
+            source={IMAGES.GOTO}
+            style={{ marginVertical: 5,
+              height: 20,
+              width: 20,
+              resizeMode: 'contain',
+            }}
+          />
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   render() {
+    const { notifications } = this.props;
     return (
       <View style={{ flex: 1, backgroundColor: COLORS.WHITE }}>
         <View style={cs.header}>
@@ -41,77 +98,15 @@ export default class NotificationsPage extends React.Component {
           />
         </View>
         <View style={{ paddingHorizontal: 8 }}>
-          <View style={{
-            marginVertical: 8,
-            borderBottomWidth: 1,
-            borderColor: COLORS.SECONDARY,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}
-          >
-            <View>
-              <Text style={{ marginLeft: 15, ...font(13), backgroundColor: COLORS.TRANSPARENT }}>
-                Aron liked your answer
-              </Text>
-              <Text style={{
-                marginLeft: 15,
-                ...font(10),
-                marginVertical: 3,
-                backgroundColor:
-                COLORS.TRANSPARENT }}
-              >
-                5 hours ago
-              </Text>
-            </View>
-            <IconButton
-              source={IMAGES.GOTO}
-              style={{ marginVertical: 5 }}
-              imageStyle={{
-                height: 20,
-                width: 20,
-                resizeMode: 'contain',
-              }}
-            />
-          </View>
-          <View style={{
-            marginVertical: 8,
-            borderBottomWidth: 1,
-            borderColor: COLORS.SECONDARY,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}
-          >
-            <View>
-              <Text style={{
-                marginLeft: 15,
-                ...font(13),
-                backgroundColor: COLORS.TRANSPARENT }}
-              >
-                 Julian liked your answer
-              </Text>
-              <Text style={{
-                marginLeft: 15,
-                ...font(10),
-                marginVertical: 3,
-                backgroundColor: COLORS.TRANSPARENT }}
-              >
-                  11 hours ago
-              </Text>
-            </View>
-            <IconButton
-              source={IMAGES.GOTO}
-              style={{ marginVertical: 5 }}
-              imageStyle={{
-                height: 20,
-                width: 20,
-                resizeMode: 'contain',
-              }}
-            />
-          </View>
+          {notifications.map(this.renderNotification)}
         </View>
       </View>
     );
   }
 }
+
+
+const mapStateToProps = ({ profile: { notifications } }) =>
+  ({ notifications });
+
+export default connect(mapStateToProps)(NotificationsPage);
