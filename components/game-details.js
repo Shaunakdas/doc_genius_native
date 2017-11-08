9cbf4ab05d7b91bc2ae18ce72b9a4b86a65c3f0c
Shaunak Das <shaunakdas2020@gmail.com>
2017-11-08 20:19:55 +0530

    game details page props set

--- components/game-details.js
+++ components/game-details.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {
   Grid,
   Col,
@@ -21,7 +22,7 @@ import {
 
 import {
   Image,
-  View
+  View,
 } from 'react-native';
 // import IMAGES from '../common/images';
 import BenifitsTab from './benifits-tab';
@@ -31,53 +32,93 @@ import ScoresTab from './scores-tab';
 import IMAGES from '../common/images';
 
 export default class GameDetails extends Component {
+  static propTypes = {
+    title: PropTypes.string,
+    gameRank: PropTypes.string,
+    bestScore: PropTypes.number,
+    challenges: PropTypes.any,
+    scores: PropTypes.any,
+    benifits: PropTypes.any,
+    onGameStart: PropTypes.func,
+  }
+  static defaultProps = {
+    title: 'Title',
+    gameRank: 'Easy',
+    bestScore: 0,
+    challenges: [
+      { icon: 'flash', title: 'Score above 1480 to rank up to Novice', description: null },
+      { icon: 'people', title: 'Train with friends!', description: 'See if you can beat their score' },
+    ],
+    benifits: [
+      { title: 'Task Shifting', description: 'Excersies your multitasking and task-switching skills' },
+      { title: 'Response Control', description: 'Challenges your ability to take a step back and think before you act' },
+    ],
+    scores: {
+      recent: [
+        { value: 333, time: '3 mins ago' },
+        { value: 112, time: '14 days ago' },
+      ],
+      top: [
+        { value: 412, time: '3 months ago' },
+        { value: 112, time: '12 secs ago' },
+      ],
+    },
+    onGameStart: null,
+  }
   render() {
+    const {
+      title,
+      gameRank,
+      bestScore,
+      challenges,
+      scores,
+      benifits,
+      onGameStart } = this.props;
     return (
       <Container style={{ marginTop: 25 }}>
-        <Header style={{  backgroundColor: '#00bfff' }}>
+        <Header style={{ backgroundColor: '#00bfff' }}>
           <Left>
             <Button transparent>
-              <Icon name='arrow-round-back' />
+              <Icon name={'arrow-round-back'} />
             </Button>
           </Left>
           <Body>
-            <Title>Speed Spotting</Title>
+            <Title>{title}</Title>
           </Body>
           <Right>
             <Button transparent>
-              <Icon name='information-circle' />
+              <Icon name={'information-circle'} />
             </Button>
           </Right>
         </Header>
         <Content>
           <Grid>
-            <Row style={ { height: 150, backgroundColor: '#00bfff'} }>
+            <Row style={{ height: 150, backgroundColor: '#00bfff' }}>
               <Image opacity={0.1} style={{ height: 150, width: null, flex: 1 }} source={IMAGES.LOGO}>
-                <View style={{flex: 1, flexDirection: 'row'}}>
-                  <Col style={{ justifyContent: 'center', alignItems: 'center'  }}>
-                    <Icon name='ribbon'/>
-                    <Title style={{ color: '#fff0f0', fontSize: 25}}> 400 </Title>
-                    <Text style={{ color: '#fff0f0'}}> BEST SCORE </Text>
+                <View style={{ flex: 1, flexDirection: 'row' }}>
+                  <Col style={{ justifyContent: 'center', alignItems: 'center' }}>
+                    <Icon name={'ribbon'} />
+                    <Title style={{ color: '#fff0f0', fontSize: 25 }}> {bestScore} </Title>
+                    <Text style={{ color: '#fff0f0' }}> BEST SCORE </Text>
                   </Col>
-                  <Col style={{ justifyContent: 'center', alignItems: 'center'  }}>
-                    <Icon name='school' />
-                    <Title style={{ color: '#fff0f0', fontSize: 25}}> BEGINNER </Title>
-                    <Text style={{ color: '#fff0f0'}}> GAME RANK </Text>
+                  <Col style={{ justifyContent: 'center', alignItems: 'center' }}>
+                    <Icon name={'school'} />
+                    <Title style={{ color: '#fff0f0', fontSize: 25 }}> {gameRank} </Title>
+                    <Text style={{ color: '#fff0f0' }}> GAME RANK </Text>
                   </Col>
                 </View>
               </Image>
-              
             </Row>
             <Row size={45}>
               <Tabs initialPage={0} >
                 <Tab heading="BENIFITS">
-                  <BenifitsTab />
+                  <BenifitsTab benifits={benifits} />
                 </Tab>
                 <Tab heading="CHALLENGES">
-                  <ChallengesTab />
+                  <ChallengesTab challenges={challenges} />
                 </Tab>
                 <Tab heading="SCORES">
-                  <ScoresTab />
+                  <ScoresTab scores={scores} />
                 </Tab>
               </Tabs>
             </Row>
@@ -85,8 +126,8 @@ export default class GameDetails extends Component {
         </Content>
         <Footer>
           <FooterTab>
-            <Button rounded>
-              <Text style={{ fontSize:20 }}>PLAY GAME</Text>
+            <Button rounded onPress={onGameStart}>
+              <Text style={{ fontSize: 20 }}>PLAY GAME</Text>
             </Button>
           </FooterTab>
         </Footer>
