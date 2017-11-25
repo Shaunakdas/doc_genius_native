57f5a6ed2d2ffb32e5ce9f2b6c7e4fbc1eb44fc2
Shaunak Das <shaunakdas2020@gmail.com>
2017-11-25 22:53:00 +0530

    First time Eject setup with expo kit. TextInput selectionColour issue, button press not working

---
+++ android/app/src/main/java/com/connecpath/forum/generated/ExponentBuildConstants.java
@@ -0,0 +1,8 @@
+package com.connecpath.forum.generated;
+
+// This file is auto-generated. Please don't rename!
+public class ExponentBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "expea75739a31a245a0ba5273b127934cab://t4-w95.shaunakdas2020.doc-genius-native.exp.direct:80";
+
+}
