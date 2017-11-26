30f1e7542cf525c2f4badf18a03293af16a81265
Shaunak Das <shaunakdas2020@gmail.com>
2017-11-26 21:54:53 +0530

    added first real device apk

--- newpages/SplashPage.js
+++ newpages/SplashPage.js
@@ -49,7 +49,7 @@ class SplashPage extends React.Component {
     //   'Roboto': require('native-base/Fonts/Roboto.ttf'),
     //   'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
     // }));
-    // this.start();
+    this.start();
   }
 
   setMessage = loadingMessage => this.setState({ loadingMessage });
